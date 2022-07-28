83b48353df4de96d728bbfd682f5ee918d79744c
Matheus Calixto <matheuscalixto8@gmail.com>
2022-07-28 18:32:40 -0300

    feat: update validations for new and current notes

--- models/note.js
+++ models/note.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const url = process.env.MONGODB_URI;
@@ -16,8 +16,15 @@ mongoose
   });
 
 const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
   important: Boolean,
 });
 
