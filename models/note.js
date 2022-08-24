e4d5a913cc743ebcd7086c766dd4b9e2a10d5a60
Matheus Calixto <matheuscalixto8@gmail.com>
2022-08-24 18:16:31 -0300

    refactor: removed unnecessary connection with database from model

--- models/note.js
+++ models/note.js
@@ -1,20 +1,5 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const config = require('../utils/config')
-
-const url = config.MONGODB_URI;
-
-console.log('connecting to', url);
-
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log('' && result);
-    console.log('connected to MongoDB');
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
-  });
 
 const noteSchema = new mongoose.Schema({
   content: {
